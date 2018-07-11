23845d3b904597d042c4efdaa6075b17b2ae12f1
verna <vernajs@gmail.com>
2018-07-10 19:59:01 -0700

    Index Test

---
+++ __test__/reducers/index-reducer.test.js
@@ -0,0 +1,12 @@
+import rootReducer from './../../src/reducers/index';
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+    masterTicketList: {},
+    selectedTicket: {}
+  });
+
+});
+});
