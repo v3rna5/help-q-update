2012fd6392a783316b25d76e3f975ea03570002b
verna <vernajs@gmail.com>
2018-07-11 16:40:33 -0700

    completed Tues lesson

--- __test__/reducers/ticket-list-reducer.test.js
+++ __test__/reducers/ticket-list-reducer.test.js
@@ -1,5 +1,6 @@
 import ticketListReducer from './../../src/reducers/ticket-list-reducer';
 import Moment from 'moment';
+import c from './../../src/constants';
 
 describe('ticketListReducer', () => {
 
@@ -19,7 +20,7 @@ describe('ticketListReducer', () => {
   test('Should successfully add new ticket data to masterTicketList', () => {
     const { names, location, issue, timeOpen, id } = sampleTicketData;
     action = {
-      type: 'ADD_TICKET',
+      type: c.ADD_TICKET,
       names: names,
       location: location,
       issue: issue,
@@ -36,28 +37,11 @@ describe('ticketListReducer', () => {
       }
     });
   });
-  test('Should add freshly-caculated Moment-formatted wait time to ticket entry', () => {
-    const { names, location, issue, timeOpen, id } = sampleTicketData;
-    action = {
-      type: 'UPDATE_TIME',
-      formattedWaitTime: '4 minutes',
-      id: id
-    };
-    expect(ticketListReducer({ [id] : sampleTicketData }, action)).toEqual({
-      [id] : {
-        names: names,
-        location: location,
-        issue: issue,
-        timeOpen: timeOpen,
-        id: id,
-        formattedWaitTime: '4 minutes'
-      }
-    });
-  });
+
   test('New ticket should include Moment-formatted wait times', () => {
     const { names, location, issue, timeOpen, id } = sampleTicketData;
     action = {
-      type: 'ADD_TICKET',
+      type: c.ADD_TICKET,
       names: names,
       location: location,
       issue: issue,
@@ -76,4 +60,24 @@ describe('ticketListReducer', () => {
       }
     });
   });
+
+  test('Should add freshly-calculated Moment-formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes',
+      id: id
+    };
+    expect(ticketListReducer({ [id] : sampleTicketData }, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes'
+      }
+    });
+  });
+
 });
